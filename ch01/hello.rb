4f9be0a68e8c5e3241f9d02cba7aa7fbf6ee60d4
chenzixin <84856@163.com>
2015-04-24 13:31:42 +0800

    add source

---
+++ ch01/hello.rb
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+cities = %w[ London
+              Oslo
+              Paris
+              Amsterdam
+              Berlin ]
+
+visited = %w[Berlin Oslo]
+
+puts 'I still need ' +
+         'to visit the ' +
+         'following cities:',
+     cities - visited
